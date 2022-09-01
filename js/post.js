c5071dedc0debf9fb6384c8d5a9c5ac438bc03e1
omarfaruknuman <omarfaruk1271@gmail.com>
2022-09-01 19:12:11 +0600

    post added

--- js/post.js
+++ js/post.js
@@ -6,18 +6,22 @@ function loadPost(){
 
 /* 
 1. get the container element where you want to add the new elements
-
+2. Create child element 
+3. set innerText or innerHTML
+4. appendChild
 */
 
 function displayPost(posts){
     const postsContainer = document.getElementById('posts-container');
     for(const post of posts){
         const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
         console.log(post);
         postDiv.innerHTML = `
             <h4>User-${post.userId}</h4>
-            <h5>Post: title</h5>
-            <p>Post Description</p>
+            <h5>Post: ${post.title}</h5>
+            <p>Post Description: ${post.body}</p>
+            <p>Id: ${post.id}</p>
         `;
         postsContainer.appendChild(postDiv);
     }
