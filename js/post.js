e8263e7e00e263be8f24766db7f833bbb9917db2
omarfaruknuman <omarfaruk1271@gmail.com>
2022-09-01 17:29:01 +0600

    updated

--- js/post.js
+++ js/post.js
@@ -1,7 +1,26 @@
 function loadPost(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayPost(data))
+}
+
+/* 
+1. get the container element where you want to add the new elements
+
+*/
+
+function displayPost(posts){
+    const postsContainer = document.getElementById('posts-container');
+    for(const post of posts){
+        const postDiv = document.createElement('div');
+        console.log(post);
+        postDiv.innerHTML = `
+            <h4>User-${post.userId}</h4>
+            <h5>Post: title</h5>
+            <p>Post Description</p>
+        `;
+        postsContainer.appendChild(postDiv);
+    }
 }
 
 loadPost();
